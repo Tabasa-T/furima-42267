45e8e2bebfa0f4c26ea56becec359a1c5151d33f
Tabasa-T <t6224946t@gmail.com>
2025-04-17 17:04:01 +0900

    修正　バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' }
+  VALID_ZENKAKU_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー－]+\z/
+  VALID_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は英字と数字の両方を含めて設定してください' }
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name, presence: true, format: { with: VALID_ZENKAKU_REGEX, message: 'は全角文字で入力してください' }
+  validates :first_name, presence: true, format: { with: VALID_ZENKAKU_REGEX, message: 'は全角文字で入力してください' }
+  validates :family_name_kana, presence: true,  format: { with: VALID_KATAKANA_REGEX, message: 'は全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true,  format: { with: VALID_KATAKANA_REGEX, message: 'は全角カタカナで入力してください' }
   validates :birthday, presence: true
 end
