b3f9140bba413de39d0dd0a679075579eda5d920
Tabasa-T <t6224946t@gmail.com>
2025-04-17 17:23:11 +0900

    修正　正常系テスト追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,13 @@
-Faker::Config.locale = :ja
-
 FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
     password              { 'abc123' }
     password_confirmation { password }
-    family_name           { Faker::Name.last_name }
-    first_name            { Faker::Name.first_name }
-    family_name_kana      { Faker::Name.last_name.tr('ぁ-ん', 'ァ-ン') }
-    first_name_kana       { Faker::Name.first_name.tr('ぁ-ん', 'ァ-ン') }
+    family_name           { '山田' }
+    first_name            { '太郎' }
+    family_name_kana      { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
     birthday              { Faker::Date.birthday(min_age: 0, max_age: 130) }
   end
 end
