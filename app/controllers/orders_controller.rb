ad3263a94c58d4a70c2e8ba5e39461c28eb09004
Tabasa-T <t6224946t@gmail.com>
2025-04-24 18:06:57 +0900

    indexアクションの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,11 @@
 class OrdersController < ApplicationController
+
+  def index
+    @order_shipping_address = OrderShippingAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+  end
+
 end
