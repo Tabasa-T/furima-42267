816f26ee91ab99dbee0c7f4ac8a125caa0dc65ea
Tabasa-T <t6224946t@gmail.com>
2025-04-25 12:43:21 +0900

    createアクションの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,36 @@
 class OrdersController < ApplicationController
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_shipping_address = OrderShippingAddress.new
     @item = Item.find(params[:item_id])
   end
 
   def create
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_shipping_address.valid?
+      pay_item
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render :index, status: :unprocessable_entity
+    end
   end
 
+  private
+
+  def order_params
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
