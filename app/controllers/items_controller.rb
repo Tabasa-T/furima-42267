44dd3b8355f746f714498662bf859233302ccb9f
Tabasa-T <t6224946t@gmail.com>
2025-04-25 15:46:56 +0900

    売却済み商品は編集できない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -47,12 +47,13 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return if current_user.id == @item.user.id
-
-    redirect_to action: :index
+    if current_user.id != @item.user.id || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def set_item
     @item = Item.find(params[:id])
   end
+
 end
